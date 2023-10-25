65e7c289a88c5f8c8a589bd961ffc75620e1ead6
Robpiot <robin.piot@outlook.com>
2023-10-25 10:39:35 +0200

    index.php is done

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang = ENG>
+<head>
+<meta name = "title" content='AZ-Store'>
+<link rel="stylesheet" href="css/style.css">
+<title>AZ-Store</title>
+</head>
+<body>
+<header>
+<nav>
+<a href="">AZ[store]</a>
+<li><a href="">Home</a></li>
+<li><a href="">About</a></li>
+<li><a href="">Products</a></li>
+<li><a href="">Contact</a></li>
+<a href="">Login</a>
+</nav>
+</header>
+<main>
+    <section>
+        <div>
+            <h2>SHOE THE RIGHT ONE.</h2>
+            <button>See our store</button>
+        </div>
+        <div>
+            <h2>NIKE</h2>
+            <img src="Ressources/shoe_one.png" alt="shoe_one">
+        </div>
+    </section>
+    <section>
+        <div><h3>Our last products</h3></div>
+        <div>
+            <article class="card">
+                <div>
+                    <img src="Ressources/shoe_one.png" alt="shoe_one">
+                </div>
+                <div>
+                    <span><h3>NIKE Air</h3></span>
+                    <span><h4>234€</h4></span>
+                    <span><button>Add to cart</button></span>
+                </div>
+            </article>
+            <article class="card">
+                <div>
+                    <img src="Ressources/shoe_one.png" alt="shoe_one">
+                </div>
+                <div>
+                    <span><h3>NIKE Air</h3></span>
+                    <span><h4>234€</h4></span>
+                    <span><button>Add to cart</button></span>
+                </div>
+            </article>
+            <article class="card">
+                <div>
+                    <img src="Ressources/shoe_one.png" alt="shoe_one">
+                </div>
+                <div>
+                    <span><h3>NIKE Air</h3></span>
+                    <span><h4>234€</h4></span>
+                    <span><button>Add to cart</button></span>
+                </div>
+            </article>
+            <article class="card">
+                <div>
+                    <img src="Ressources/shoe_one.png" alt="shoe_one">
+                </div>
+                <div>
+                    <span><h3>NIKE Air</h3></span>
+                    <span><h4>234€</h4></span>
+                    <span><button>Add to cart</button></span>
+                </div>
+            </article>
+        </div>
+    </section>
+    <section>
+        <div>
+        <img src="Ressources/shoe_one.png" alt="shoe_one">
+        <h3>WE PROVIDE YOU THE BESTQUALITY</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, esse quis ab ullam magni sit corporis, quam, quibusdam illum quaerat provident vero.</p>
+        </div>
+    </section>
+    <section>
+        <div class="cardBis">
+            <img src="Ressources/image-emily.jpg" alt="Emily">
+            <h4>Emily from xyz</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, facere! Molestiae impedit nostrum possimus!</p>
+        </div>
+        <div class="cardBis">
+            <img src="Ressources/image-thomas.jpg" alt="Thomas">
+            <h4>Thomas from corporate</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, facere! Molestiae impedit nostrum possimus!</p>
+        </div>
+        <div class="cardBis">
+            <img src="Ressources/image-jennie.jpg" alt="Jennie">
+            <h4>Jennie from Nike</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, facere! Molestiae impedit nostrum possimus!</p>
+        </div>
+    </section>
+</main>
+<footer>
+    <ul>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Product</a>
+        <a href="">Contact</a>
+    </ul>
+</footer>
+</body>
